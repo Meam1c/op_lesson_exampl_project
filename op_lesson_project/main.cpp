b22faeadc0f6f0f77f41370bf712c3a47ec50bad
Meam1c <matvey.jilkevich@gmail.com>
2023-03-30 17:38:33 +0300

    Add project files.

---
+++ op_lesson_project/main.cpp
@@ -0,0 +1,46 @@
+#include"main.h"
+
+class Student {
+public:
+	string name ;
+	string surname;
+	int age;
+	float avg_mark;
+};
+
+
+
+int main() {
+	Student st1;
+	st1.name="ivan";
+	st1.surname="ivanov";
+	st1.age = 14;
+	st1.avg_mark = 10;
+
+	Student st2 = st1;
+
+	cout << "Defore:" << endl;
+	cout << st1.name << endl;
+	cout << st1.surname << endl;
+	cout << st1.age << endl;
+	cout << st1.avg_mark << endl;
+
+	cout << st2.name << endl;
+	cout << st2.surname << endl;
+	cout << st2.age << endl;
+	cout << st2.avg_mark << endl;
+
+	st1.avg_mark = 7;
+
+	cout << "\nAfter:" << endl;
+	cout << st1.name << endl;
+	cout << st1.surname << endl;
+	cout << st1.age << endl;
+	cout << st1.avg_mark << endl;
+
+	cout << st2.name << endl;
+	cout << st2.surname << endl;
+	cout << st2.age << endl;
+	cout << st2.avg_mark << endl;
+	return 0;
+}
