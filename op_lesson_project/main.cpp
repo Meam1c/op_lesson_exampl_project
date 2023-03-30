1d2de2b6f22adc108ef1bbb3533e68be288ffde9
Meam1c <matvey.jilkevich@gmail.com>
2023-03-30 18:41:13 +0300

    добавление методов в класс

--- op_lesson_project/main.cpp
+++ op_lesson_project/main.cpp
@@ -6,15 +6,25 @@ public:
 	string surname;
 	int age;
 	float avg_mark;
+
+	void clear() {
+		name = "no name";
+		surname = "no surname";
+		age = 0;
+		avg_mark = 0;
+
+	}
+
+	string convert() {
+		string msg = "";
+		msg += name;
+		msg += " " + surname;
+		msg += " (age = " + to_string(age);
+		msg += " , average mark =  " + to_string(avg_mark);
+		msg += " )";
+		return msg;
+	}
 };
-
-void clear(Student st) {
-	st.name = "no name";
-	st.surname = "no surname";
-	st.age = 0;
-	st.avg_mark = 0;
-
-}
 
 int main() {
 	Student st1;
@@ -26,19 +36,12 @@ int main() {
 	Student st2 = st1;
 
 	cout << "Defore:" << endl;
-	cout << st1.name << endl;
-	cout << st1.surname << endl;
-	cout << st1.age << endl;
-	cout << st1.avg_mark << endl;
+	cout << st1.convert() << endl;
 
-	clear(st1);
+	st1.clear();
 
 	cout << "\nAfter:" << endl;
-	cout << st1.name << endl;
-	cout << st1.surname << endl;
-	cout << st1.age << endl;
-	cout << st1.avg_mark << endl;
+	cout << st1.convert() << endl;
 
-	
 	return 0;
 }
--- op_lesson_project/main.h
+++ op_lesson_project/main.h
@@ -1,3 +1,3 @@
 #include<iostream>
-#include<iostream>
+#include<string>
 using namespace std;
